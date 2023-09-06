96e5c3f3f792798b90c351c4a87f81a6edd5ae65
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2023-09-06 08:58:34 +0200

    chore: add demo file

---
+++ demoFiles/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+// We need to add this code manually
+import { ConfigModule } from '@nestjs/config';
+import config, { validate } from './config/configuration';
+
+@Module({
+  imports: [
+    // We need to add this code manually
+    // ConfigModule.forRoot({
+    //   isGlobal: true,
+    //   load: [config],
+    //   validate,
+    // }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
