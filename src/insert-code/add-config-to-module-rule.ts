051879b6fd335abe93222de590edaa34fef4f303
David Yusta <david.yusta@guidesmiths.com>
2023-09-12 11:34:29 +0200

    refactor: commit all changes to tree

--- src/insert-code/add-config-to-module-rule.ts
+++ src/insert-code/add-config-to-module-rule.ts
@@ -1,12 +1,20 @@
-import { Rule, Tree } from "@angular-devkit/schematics";
-import { addConfigToModuleChange } from '../utils/add-config-to-module-change';
+import {Rule, SchematicsException, Tree} from "@angular-devkit/schematics";
+import {addConfigToModuleChange} from '../utils/add-config-to-module-change';
+import {addImportsToFileChange} from '../utils/add-imports-to-file-change';
 
 export function addConfigToModuleRule(_options: any): Rule {
   return (tree: Tree) => {
     const path = './demoFiles/app.module.ts';
-    let change = addConfigToModuleChange({path}, tree);
+
+    let sourceText = tree.readText(path);
+    if (!sourceText) throw new SchematicsException(`File does not exist.`);
+
+    const importsChange = addImportsToFileChange(sourceText, {path});
+    const moduleChange = addConfigToModuleChange(sourceText, {path});
+
     const declarationRecorder = tree.beginUpdate(path);
-    declarationRecorder.insertLeft(change.pos, change.toAdd);
+    declarationRecorder.insertLeft(importsChange.pos, importsChange.toAdd);
+    declarationRecorder.insertLeft(moduleChange.pos, moduleChange.toAdd);
     tree.commitUpdate(declarationRecorder);
 
     return tree;
